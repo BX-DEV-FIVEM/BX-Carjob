0ed31f230aa1d332ab5904fc340252078dd07e1b
BX-DEV <140925178+BX-DEV-FIVEM@users.noreply.github.com>
2024-05-05 13:58:35 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,12 +4,12 @@ game 'gta5'
 lua54 'yes'
 
 
-name 'CARJOB'
+name 'BX-Carjob'
 author 'BX-DEV'
 
 description 'Script to allow access to certain vehicles based on players jobs.'
 version 'V1.0'
-url 'https://github.com/xB3NDO'
+url 'https://github.com/BX-DEV-FIVEM'
 
 server_script {
     'config.lua',
